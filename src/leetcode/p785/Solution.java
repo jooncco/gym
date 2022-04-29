be4e4f015c34e314b3e2449a9e2fd29cb66e16d9
jooncco <jooncco.g@gmail.com>
2022-04-29 12:34:33 +0900

    :sparkles: solved: p785

---
+++ src/leetcode/p785/Solution.java
@@ -0,0 +1,57 @@
+package leetcode.p785;
+// https://leetcode.com/problems/is-graph-bipartite/
+
+public class Solution {
+
+    class UnionFind {
+        private int[] group, rank;
+
+        public UnionFind(int n) {
+            group= new int[n];
+            rank= new int[n];
+            for (int i=0; i < n; ++i) group[i]= i;
+        }
+
+        public int find(int node) {
+            if (group[node] != node) {
+                group[node]= find(group[node]);
+            }
+            return group[node];
+        }
+
+        public void union(int node1, int node2) {
+            int group1= find(node1);
+            int group2= find(node2);
+
+            if (rank[group1] > rank[group2]) {
+                group[group2]= group1;
+            }
+            else if (rank[group2] > rank[group1]) {
+                group[group1]= group2;
+            }
+            else {
+                group[group1]= group2;
+                rank[group2]++;
+            }
+        }
+    }
+
+    public boolean isBipartite(int[][] graph) {
+        int n= graph.length;
+        UnionFind uf = new UnionFind(n);
+
+        for (int node=0; node < n; ++node) {
+            int[] adjNodes= graph[node];
+            for (int i=0; i < adjNodes.length; ++i) {
+                // this adjacent node already in same group?
+                if (uf.find(node) == uf.find(adjNodes[i])) return false;
+
+                // union adjacent nodes into one group
+                if (i > 0) {
+                    uf.union(adjNodes[i-1], adjNodes[i]);
+                }
+            }
+        }
+        return true;
+    }
+}
