580f07a04de225cbffb9ba86b96702c1ba4beb53
jooncco <jooncco.g@gmail.com>
2022-05-11 22:17:05 +0900

    :sparkles: solved: p1641

---
+++ src/leetcode/p1641/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.p1641;
+// https://leetcode.com/problems/count-sorted-vowel-strings/
+
+public class Solution {
+    private int ans;
+
+    public int countVowelStrings(int n) {
+        ans= 0;
+        count(0, n);
+        return ans;
+    }
+
+    private void count(int idx, int n) {
+        if (idx == 5) {
+            if (n == 0) ++ans;
+            return;
+        }
+
+        for (int i=0; i <= n; ++i) {
+            count(idx+1, n-i);
+        }
+    }
+}
