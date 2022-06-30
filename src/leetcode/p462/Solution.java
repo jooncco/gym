7669dbaafdff6c03aed10166d1ee89fbbdda67c2
jooncco <jooncco.g@gmail.com>
2022-06-30 18:03:33 +0900

    :sparkles: solved: p462

---
+++ src/leetcode/p462/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.p462;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int minMoves2(int[] nums) {
+        int n= nums.length;
+        Arrays.sort(nums);
+        int median= nums[n/2], ans= 0;
+        for (final int num : nums) {
+            ans += Math.abs(num-median);
+        }
+        return ans;
+    }
+}
