31409c05a0de17baf13742469ea989b7f15c9c35
jooncco <jooncco.g@gmail.com>
2022-07-28 13:19:25 +0900

    :sparkles: solved: p242

---
+++ src/leetcode/p242/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.p242;
+// https://leetcode.com/problems/valid-anagram/
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        // same length?
+        int n= s.length(), m= t.length();
+        if (n != m) return false;
+
+        // populate char frequency vector
+        int[] charFrequency= new int[26];
+        for (int i=0; i < n; ++i) ++charFrequency[s.charAt(i)-'a'];
+
+        // validate
+        for (int i=0; i < m; ++i) {
+            --charFrequency[t.charAt(i)-'a'];
+            if (charFrequency[t.charAt(i)-'a'] < 0) return false;
+        }
+        for (int i=0; i < 26; ++i) {
+            if (charFrequency[i] != 0) return false;
+        }
+        return true;
+    }
+}
