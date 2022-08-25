07da339bbd4480e5d56858d4d9e2a796cebbcd5e
jooncco <jooncco.g@gmail.com>
2022-08-25 09:55:30 +0900

    :sparkles: solved: p383

---
+++ src/leetcode/p383/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.p383;
+// https://leetcode.com/problems/ransom-note/
+
+public class Solution {
+    private final int NUM_OF_LETTERS = 26;
+
+    public boolean canConstruct(final String ransomNote, final String magazine) {
+        int[] ransomNoteLetterCnt = countLetters(ransomNote);
+        int[] magazineLetterCnt = countLetters(magazine);
+        for (int i=0; i < NUM_OF_LETTERS; ++i) {
+            if (ransomNoteLetterCnt[i] > magazineLetterCnt[i]) return false;
+        }
+        return true;
+    }
+
+    private int[] countLetters(final String str) {
+        int[] cnt = new int[NUM_OF_LETTERS];
+        for (char c : str.toCharArray()) {
+            ++cnt[c-'a'];
+        }
+        return cnt;
+    }
+}
