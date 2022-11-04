9eafadcc0e9f09149cb5ccf9348d704c3ed9eeb0
jooncco <jooncco.g@gmail.com>
2022-11-04 12:34:07 +0900

    :sparkles: solved: p345

---
+++ src/leetcode/p345/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.p345;
+// https://leetcode.com/problems/reverse-vowels-of-a-string/
+
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+    public String reverseVowels(String s) {
+        Stack<Character> vowels= new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) vowels.push(c);
+        }
+        StringBuilder sb= new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) sb.append(vowels.pop());
+            else sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    private boolean isVowel(final char c) {
+        return List.of('a','e','i','o','u','A','E','I','O','U').contains(c);
+    }
+}
