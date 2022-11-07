4f0fe7c444e4b96390c70dee5e1876c43efec75a
jooncco <jooncco.g@gmail.com>
2022-11-07 09:50:51 +0900

    :sparkles: solved: p1323

---
+++ src/leetcode/p1323/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.p1323;
+// https://leetcode.com/problems/maximum-69-number/
+
+public class Solution {
+    public int maximum69Number (int num) {
+        String str= String.valueOf(num);
+        int ans= 0, flag= 0;
+        for (char c : str.toCharArray()) {
+            if (c == '6') {
+                ans += flag == 0 ? 9 : 6;
+                flag= 1;
+            } else ans += 9;
+            ans *= 10;
+        }
+        return ans;
+    }
+}
