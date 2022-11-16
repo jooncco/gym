ca68c07943d8f0e00df532fd4d8a1fce1918acff
jooncco <jooncco.g@gmail.com>
2022-11-16 12:19:07 +0900

    :sparkles: solved: p374

---
+++ src/leetcode/p374/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.p374;
+// https://leetcode.com/problems/guess-number-higher-or-lower/
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int l= 1, r= n, myGuess= (l+r)/2;
+        int queryResult;
+        do {
+            queryResult= guess(myGuess);
+            if (queryResult < 0) r= myGuess-1;
+            if (queryResult > 0) l= myGuess+1;
+            myGuess= (l + r)/2;
+        } while (queryResult != 0);
+        return myGuess;
+    }
+}
+
+class GuessGame {
+    // mocked method
+    protected int guess(int num) { return 1; }
+}
