462ab3d90c045fa53a83cda7f59ddb0e314a1e1d
jooncco <jooncco.g@gmail.com>
2022-12-13 16:06:01 +0900

    :sparkles: solved: p931

--- src/leetcode/p931/Solution.java
+++ src/leetcode/p931/Solution.java
@@ -1,7 +1,20 @@
 package leetcode.p931;
+// https://leetcode.com/problems/minimum-falling-path-sum/
 
 public class Solution {
     public int minFallingPathSum(int[][] matrix) {
-
+        int n= matrix.length;
+        for (int r=1; r < n; ++r) {
+            for (int c=0; c < n; ++c) {
+                int minVal= Integer.MAX_VALUE;
+                if (c > 0) minVal= Math.min(minVal, matrix[r-1][c-1] + matrix[r][c]);
+                minVal= Math.min(minVal, matrix[r-1][c] + matrix[r][c]);
+                if (c < n-1) minVal= Math.min(minVal, matrix[r-1][c+1] + matrix[r][c]);
+                matrix[r][c]= minVal;
+            }
+        }
+        int ans= Integer.MAX_VALUE;
+        for (int c=0; c < n; ++c) ans= Math.min(ans, matrix[n-1][c]);
+        return ans;
     }
 }
