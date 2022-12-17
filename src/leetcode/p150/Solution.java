fd452285a5ec24a3fb90f94fc0af36068aeeaa7a
jooncco <jooncco.g@gmail.com>
2022-12-17 12:56:12 +0900

    :sparkles: solved: p150

---
+++ src/leetcode/p150/Solution.java
@@ -0,0 +1,30 @@
+package leetcode.p150;
+// https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+import java.util.Stack;
+
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack= new Stack<>();
+
+        int a, b;
+        for (String token : tokens) {
+            if ("+".equals(token)) {
+                a= stack.pop(); b= stack.pop();
+                stack.push(a+b);
+            } else if ("-".equals(token)) {
+                b= stack.pop(); a= stack.pop();
+                stack.push(a-b);
+            } else if ("/".equals(token)) {
+                b= stack.pop(); a= stack.pop();
+                stack.push(a/b);
+            } else if ("*".equals(token)) {
+                a= stack.pop(); b= stack.pop();
+                stack.push(a*b);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+}
