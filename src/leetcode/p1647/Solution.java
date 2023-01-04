13c861a052e7f5d9911423d2fbe9459770c80eac
jooncco <jooncco.g@gmail.com>
2023-01-04 11:18:00 +0900

    :pencil2: add link: p1647

--- src/leetcode/p1647/Solution.java
+++ src/leetcode/p1647/Solution.java
@@ -1,18 +1,18 @@
 package leetcode.p1647;
+// https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/
 
 import java.util.Arrays;
 
 public class Solution {
-    private int[] freq;
 
     public int minDeletions(String s) {
-        freq= new int[26];
+        int[] freq = new int[26];
         int n= s.length();
         for (int i=0; i < n; ++i) ++freq[s.charAt(i)-'a'];
         Arrays.sort(freq);
         int ans= 0;
         boolean[] takenFreq= new boolean[freq[25]+1];
-        for (int i=25; i >= 0 && freq[i] > 0; --i) {
+        for (int i = 25; i >= 0 && freq[i] > 0; --i) {
             if (takenFreq[freq[i]]) {
                 int curFreq= freq[i];
                 while (curFreq > 0 && takenFreq[curFreq]) {
