6d4be679f1445fa238c4f261db8cbf039219e896
jooncco <jooncco.g@gmail.com>
2023-01-06 13:14:01 +0900

    :sparkles: solved: p1833

---
+++ src/leetcode/p1833/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.p1833;
+// https://leetcode.com/problems/maximum-ice-cream-bars/
+
+import java.util.Arrays;
+
+public class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int bars= 0;
+        for (int cost : costs) {
+            if (coins < cost) break;
+            coins -= cost;
+            ++bars;
+        }
+        return bars;
+    }
+}
