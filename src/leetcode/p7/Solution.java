c87e5935a77a021e8a4e95d2734ba72784d483b7
jooncco <jooncco.g@gmail.com>
2023-01-22 09:12:23 +0900

    :sparkles: solved: p7

---
+++ src/leetcode/p7/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.p7;
+// https://leetcode.com/problems/reverse-integer/
+
+public class Solution {
+    public int reverse(int x) {
+        String strX= String.valueOf(x);
+        StringBuilder sb= new StringBuilder();
+        if (strX.charAt(0) == '-') {
+            sb.append('-');
+            strX= strX.substring(1);
+        }
+        for (int i=strX.length()-1; i >= 0; --i) sb.append(strX.charAt(i));
+        int ret;
+        try {
+            ret= Integer.parseInt(sb.toString());
+        } catch (NumberFormatException ex) {
+            return 0;
+        }
+        return ret;
+    }
+}
