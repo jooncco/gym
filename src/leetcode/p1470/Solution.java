ca5c103b4ea737b837f4babb612d1a028e2ee293
jooncco <jooncco.g@gmail.com>
2023-02-06 16:21:16 +0900

    :sparkles: solved: p1470

---
+++ src/leetcode/p1470/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.p1470;
+// https://leetcode.com/problems/shuffle-the-array/
+
+public class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ret= new int[2*n];
+        for (int i=0; i < n; ++i) {
+            ret[i*2]= nums[i];
+        }
+        for (int i=n; i < 2*n; ++i) {
+            ret[(i-n)*2+1]= nums[i];
+        }
+        return ret;
+    }
+}
