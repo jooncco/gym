7bee46a8b02830cd2188dd49ead7a7de0e8ec33a
jooncco <jooncco.g@gmail.com>
2023-02-09 16:09:26 +0900

    :recycle: refactor: variable name

--- src/leetcode/p2306/Solution.java
+++ src/leetcode/p2306/Solution.java
@@ -6,22 +6,22 @@ import java.util.Set;
 
 public class Solution {
     public long distinctNames(String[] ideas) {
-        Set<String>[] suffixStartWith= new HashSet[26];
+        Set<String>[] suffixes= new HashSet[26];
         for (int i=0; i < 26; ++i) {
-            suffixStartWith[i]= new HashSet<>();
+            suffixes[i]= new HashSet<>();
         }
         for (String idea : ideas) {
-            suffixStartWith[idea.charAt(0)-'a'].add(idea.substring(1));
+            suffixes[idea.charAt(0)-'a'].add(idea.substring(1));
         }
         long ans= 0;
         for (int i=0; i < 26; ++i) {
             for (int j=i+1; j < 26; ++j) {
                 long cntA= 0, cntB= 0;
-                for (String suffix : suffixStartWith[i]) {
-                    if (!suffixStartWith[j].contains(suffix)) ++cntA;
+                for (String suffix : suffixes[i]) {
+                    if (!suffixes[j].contains(suffix)) ++cntA;
                 }
-                for (String suffix : suffixStartWith[j]) {
-                    if (!suffixStartWith[i].contains(suffix)) ++cntB;
+                for (String suffix : suffixes[j]) {
+                    if (!suffixes[i].contains(suffix)) ++cntB;
                 }
                 ans += cntA*cntB;
             }
