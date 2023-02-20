da9de3e78675e340da04fb1e168755b463798529
jooncco <jooncco.g@gmail.com>
2023-02-20 12:55:01 +0900

    :sparkles: solved: p35

---
+++ src/leetcode/p35/Solution.java
@@ -0,0 +1,14 @@
+package leetcode.p35;
+// https://leetcode.com/problems/search-insert-position/
+
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l= 0, r= nums.length;
+        while (l < r) {
+            int m= l + (r-l)/2;
+            if (nums[m] < target) l= m+1;
+            else r= m;
+        }
+        return l;
+    }
+}
