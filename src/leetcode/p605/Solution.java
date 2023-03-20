71330948c61f1b25109859df85a38273cb1f1cbe
jooncco <jooncco.g@gmail.com>
2023-03-20 22:39:50 +0900

    :sparkles: solved: p605

---
+++ src/leetcode/p605/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.p605;
+// https://leetcode.com/problems/can-place-flowers/
+
+public class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int cnt= 0;
+        for (int i=0; i < n; ++i) {
+            if ((i == 0 || flowerbed[i-1] == 0) && flowerbed[i] == 0 && (i == n-1 || flowerbed[i+1] == 0)) {
+                flowerbed[i]= 1;
+                ++cnt;
+            }
+        }
+        return cnt >= n;
+    }
+}
