598a4e9ee06967a41f205836cab8d3ae01108cf8
jooncco <jooncco.g@gmail.com>
2023-03-28 12:59:10 +0900

    :sparkles: solved: p983

---
+++ src/leetcode/p983/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.p983;
+// https://leetcode.com/problems/minimum-cost-for-tickets/
+
+public class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        final int N= days.length;
+        final int INF= 365*1000+1;
+        final int[] PASS_LEN= {1, 7, 30};
+
+        int[] cache= new int[N+1];
+        for (int i=1; i <= N; ++i) cache[i]= INF;
+        for (int i=1; i <= N; ++i) {
+            for (int j=0; j < 3; ++j) {
+                int cost= costs[j];
+                int passExpirationDate= days[i-1]+PASS_LEN[j];
+                int idx= i;
+                while (idx <= N && days[idx-1] < passExpirationDate) {
+                    cache[idx]= Math.min(cache[idx], cache[i-1]+cost);
+                    ++idx;
+                }
+            }
+        }
+        return cache[N];
+    }
+}
