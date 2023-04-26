48df4e3daf003d9d89baf95ed40bbe4d68321d6d
jooncco <jooncco.g@gmail.com>
2023-04-26 11:02:10 +0900

    :sparkles: solved: p258

---
+++ src/leetcode/p258/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.p258;
+// https://leetcode.com/problems/add-digits/description/
+
+/**
+ * Math
+ * | Time: O (log num)
+ * | Space: O (1)
+ */
+public class Solution {
+    public int addDigits(int num) {
+        int ans= num;
+        while (ans > 9) {
+            int n= ans;
+            ans= 0;
+            while (n > 0) {
+                ans += n%10;
+                n /= 10;
+            }
+        }
+        return ans;
+    }
+}
