7efb16092ec89d196e2a3501d49d04a9593e26c1
jooncco <jooncco.g@gmail.com>
2023-04-27 13:08:45 +0900

    :sparkles: solved: p319

---
+++ src/leetcode/p319/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.p319;
+// https://leetcode.com/problems/bulb-switcher/submissions/940396427/
+
+/**
+ * Math
+ * | Time: O (log n)
+ * | Space: O (1)
+ */
+public class Solution {
+    public int bulbSwitch(int n) {
+        int i= 1;
+        while (i*i <= n) ++i;
+        return i;
+    }
+}
