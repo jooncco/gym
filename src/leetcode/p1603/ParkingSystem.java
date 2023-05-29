4e09a665b3be6b15d3930419354848e54673a746
jooncco <jooncco.g@gmail.com>
2023-05-29 15:57:11 +0900

    :sparkles: solved: p1603

---
+++ src/leetcode/p1603/ParkingSystem.java
@@ -0,0 +1,22 @@
+package leetcode.p1603;
+
+/**
+ * Ad hoc
+ * | Time: O (1)
+ * | Space: O (1)
+ */
+public class ParkingSystem {
+    private final int[] slots= new int[4], occupied= new int[4];
+
+    public ParkingSystem(int big, int medium, int small) {
+        slots[1]= big;
+        slots[2]= medium;
+        slots[3]= small;
+    }
+
+    public boolean addCar(int carType) {
+        if (occupied[carType] == slots[carType]) return false;
+        ++occupied[carType];
+        return true;
+    }
+}
