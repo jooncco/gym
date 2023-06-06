227273411dbbeac8da0e7ca82a708aa5520e30eb
jooncco <jooncco.g@gmail.com>
2023-06-06 12:11:18 +0900

    :sparkles: solved: p1502

---
+++ src/leetcode/p1502/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.p1502;
+// https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
+
+import java.util.Arrays;
+
+/**
+ * Sorting
+ * | Time: O (n log n)
+ * | Space: O (1)
+ */
+public class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+		int diff= arr[1]-arr[0];
+		for (int i=1; i < arr.length; ++i) {
+			if (arr[i]-arr[i-1] != diff) return false;
+		}
+		return true;
+    }
+}
