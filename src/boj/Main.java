a4309d10fcce63676a974f973e8c41a27c0599e9
jooncco <jooncco.g@gmail.com>
2023-07-27 10:24:57 +0900

    :recycle: refactor: cp template

--- src/boj/Main.java
+++ src/boj/Main.java
@@ -6,31 +6,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private final static FastScanner sc= new FastScanner();
-    private static int t;
+    private static String solve() throws IOException {
+        return "";
+    }
+
+    private static class Sc {
+        private static BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        private static StringTokenizer st= new StringTokenizer("");
+        static String next() throws IOException {
+            while (!st.hasMoreTokens()) {
+                st= new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+        static long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+    }
 
     public static void main(String[] args) throws IOException {
-        t= sc.nextInt();
-        while (t-- > 0) {
-
+        StringBuilder sb= new StringBuilder();
+        int T= Sc.nextInt();
+        for (int t=1; t <= T; ++t) {
+            if (t > 1) sb.append("\n");
+            sb.append("Case #").append(t).append(": ");
+            sb.append(solve());
         }
-    }
-}
-
-class FastScanner {
-    BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st= new StringTokenizer("");
-    String next() throws IOException {
-        while (!st.hasMoreTokens()) {
-            st= new StringTokenizer(br.readLine());
-        }
-        return st.nextToken();
-    }
-
-    int nextInt() throws IOException {
-        return Integer.parseInt(next());
-    }
-    long nextLong() throws IOException {
-        return Long.parseLong(next());
+        System.out.print(sb);
     }
 }
