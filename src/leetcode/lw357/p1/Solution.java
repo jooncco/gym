05e878acd2b181f9e523f5d92cd2ebb14856908c
jooncco <jooncco.g@gmail.com>
2023-08-06 15:24:41 +0900

    :sparkles: solved: lw357 - p1, p2, p3(, p4)

---
+++ src/leetcode/lw357/p1/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.lw357.p1;
+// https://leetcode.com/contest/weekly-contest-357/problems/faulty-keyboard/
+
+/**
+ * Implementation (String)
+ * | Time: O (n^n)
+ * | Space: O (n)
+ */
+public class Solution {
+    public String finalString(String s) {
+        StringBuilder sb= new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == 'i') sb.reverse();
+            else sb.append(c);
+        }
+        return sb.toString();
+    }
+}
