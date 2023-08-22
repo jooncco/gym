1d6259f57ac97bc1453d9b22146ea090fe6f8c55
jooncco <jooncco.g@gmail.com>
2023-08-22 10:00:06 +0900

    :sparkles: solved: p168

---
+++ src/leetcode/p168/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.p168;
+// https://leetcode.com/problems/excel-sheet-column-title/description/
+
+/**
+ * Math (Radix)
+ * | Time: O (log(n))
+ * | Space: O (1)
+ */
+public class Solution {
+    public String convertToTitle(int columnNumber) {
+        String ret= "";
+        while (columnNumber > 0) {
+            ret= ((char)('A'+(columnNumber-1)%26)) + ret;
+            columnNumber= (columnNumber-1) / 26;
+        }
+        return ret;
+    }
+}
