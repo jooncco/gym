cc904ed33adff3ed97f8177a4bfb14bc3f312fc0
jooncco <jooncco.g@gmail.com>
2023-09-07 13:02:49 +0900

    :sparkles: solved: p92

--- src/leetcode/p92/Solution.java
+++ src/leetcode/p92/Solution.java
@@ -1,37 +1,43 @@
 package leetcode.p92;
 // https://leetcode.com/problems/reverse-linked-list-ii/
 
-import java.util.Stack;
-
+/**
+ * Implementation
+ * | Time: O(n)
+ * | Space: O(1)
+ */
 public class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        Stack<ListNode> stack = new Stack<>();
-        ListNode iteratorNode = new ListNode();
-        ListNode virtualHead = iteratorNode;
-        ListNode curNode = head;
-        int curIdx = 1;
-        while (curIdx < left) {
-            iteratorNode.next = curNode;
-            iteratorNode = iteratorNode.next;
-            curNode= curNode.next;
-            ++curIdx;
+        if (left == right) return head;
+
+        int idx= 0;
+        ListNode virHead= new ListNode();
+        virHead.next= head;
+        ListNode cutPrev= virHead, tmp= null, tmp2;
+        ListNode cur= virHead;
+        while (cur != null) {
+            if (idx <= left-1) {
+                if (idx == left-1) cutPrev= cur;
+                cur= cur.next;
+            }
+            if (idx == left) {
+                tmp= cur;
+                cur= cur.next;
+                tmp.next= null;
+            }
+            if (idx > left && idx <= right) {
+                tmp2= cur;
+                cur= cur.next;
+                tmp2.next= tmp;
+                tmp= tmp2;
+            }
+            if (idx > right) break;
+            ++idx;
         }
-        while (curIdx <= right) {
-            stack.push(curNode);
-            curNode= curNode.next;
-            ++curIdx;
-        }
-        while (!stack.isEmpty()) {
-            iteratorNode.next = stack.pop();
-            iteratorNode = iteratorNode.next;
-        }
-        while (curNode != null) {
-            iteratorNode.next = curNode;
-            iteratorNode = iteratorNode.next;
-            curNode= curNode.next;
-        }
-        iteratorNode.next= null;
-        return virtualHead.next;
+        cutPrev.next= tmp;
+        while (tmp.next != null) tmp= tmp.next;
+        tmp.next= cur;
+        return virHead.next;
     }
 }
 
