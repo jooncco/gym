b0532cb52c229cfa1c07f9cef43678e4079b485f
jooncco <jooncco.g@gmail.com>
2023-09-08 15:18:35 +0900

    :sparkles: solved: p118

--- src/leetcode/p118/Solution.java
+++ src/leetcode/p118/Solution.java
@@ -1,22 +1,27 @@
 package leetcode.p118;
+// https://leetcode.com/problems/pascals-triangle/
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Implementation
+ * | Time: O(n^2)
+ * | Space: O(n^2)
+ */
 public class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> pascalsTriangle= new ArrayList<>();
-        pascalsTriangle.add(List.of(1));
-        while (pascalsTriangle.size() < numRows) {
-            List<Integer> prevRow= pascalsTriangle.get(pascalsTriangle.size()-1);
-            List<Integer> newRow= new ArrayList<>();
-            newRow.add(1);
-            for (int idx=1; idx < prevRow.size(); ++idx) {
-                newRow.add(prevRow.get(idx-1) + prevRow.get(idx));
+        List<List<Integer>> list= new ArrayList<>();
+        list.add(List.of(1));
+        for (int i=1; i < numRows; ++i) {
+            List<Integer> row= new ArrayList<>();
+            row.add(1);
+            for (int j=1; j < i; ++j) {
+                row.add(list.get(i-1).get(j-1) + list.get(i-1).get(j));
             }
-            newRow.add(1);
-            pascalsTriangle.add(newRow);
+            row.add(1);
+            list.add(row);
         }
-        return pascalsTriangle;
+        return list;
     }
 }
