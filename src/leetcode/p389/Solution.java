0718cd2ec5b7f7866422a851b864b930deaeb9f8
jooncco <jooncco.g@gmail.com>
2023-09-25 10:39:59 +0900

    :sparkles: solved: p389

---
+++ src/leetcode/p389/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.p389;
+// https://leetcode.com/problems/find-the-difference/
+
+/**
+ * Brute Force
+ * | Time: O(n)
+ * | Space: O(1)
+ */
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] cnt= new int[26];
+        for (char c : s.toCharArray()) {
+            ++cnt[c-'a'];
+        }
+        for (char c : t.toCharArray()) {
+            --cnt[c-'a'];
+            if (cnt[c-'a'] < 0) {
+                return c;
+            }
+        }
+        return '-';
+    }
+}
