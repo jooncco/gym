f8a926696fb4bb0b4c25f6ee9736c93e0742c2ba
jooncco <jooncco.g@gmail.com>
2023-10-02 13:54:18 +0900

    :sparkles: solved: p2038

---
+++ src/leetcode/p2038/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.p2038;
+// https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/
+
+/**
+ * Game Theory
+ * | Time: O(|colors|)
+ * | Space: O(1)
+ */
+public class Solution {
+    public boolean winnerOfGame(String colors) {
+        int n= colors.length();
+        int a= 0, b= 0;
+        for (int i= 1; i < n-1; ++i) {
+            if (colors.charAt(i) == 'A' && colors.charAt(i-1) == 'A' && colors.charAt(i+1) == 'A') {
+                ++a;
+            }
+            if (colors.charAt(i) == 'B' && colors.charAt(i-1) == 'B' && colors.charAt(i+1) == 'B') {
+                ++b;
+            }
+        }
+        if (a == 0) return false;
+        return a > b;
+    }
+}
