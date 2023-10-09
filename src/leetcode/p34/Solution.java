2c0bce38d85d16138bde694e51768afd2549c5eb
jooncco <jooncco.g@gmail.com>
2023-10-09 21:54:21 +0900

    :sparkles: solved: p34

--- src/leetcode/p34/Solution.java
+++ src/leetcode/p34/Solution.java
@@ -1,33 +1,26 @@
 package leetcode.p34;
 // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 
+/**
+ * Binary Search
+ * | Time: O(log(n))
+ * | Space: O(1)
+ */
 public class Solution {
-    private int n;
-    private int[] notFound= {-1, -1};
-    private int[] trivialSolution= {0, 0};
-
     public int[] searchRange(int[] nums, int target) {
-        this.n= nums.length;
-        if (n == 0) return notFound;
-        if (n == 1) return nums[0] == target ? trivialSolution : notFound;
+        if (nums.length == 0) return new int[]{-1,-1};
 
-        // First index: normal binary search.
-        int firstIdx= binarySearch(nums, target);
-        if (nums[firstIdx] != target) return notFound;
-
-        // Last index: find (target+1) and index--
-        // Exception: if no (target+1) is found, index 'l' will be the last index (n-1).
-        int lastIdx= binarySearch(nums, target+1);
-        if (nums[lastIdx] != target+1) --lastIdx;
-
-        int[] ans= { firstIdx, lastIdx };
-        return ans;
+        int l= binarySearch(nums, target);
+        if (nums[l] != target) return new int[]{-1, -1};
+        int r= binarySearch(nums, target+1);
+        if (nums[r] != target) --r;
+        return new int[]{l, r};
     }
 
-    private int binarySearch(final int[] nums, final int target) {
-        int l= 0, r= n-1;
+    private static int binarySearch(int[] nums, int target) {
+        int l= 0, r= nums.length-1;
         while (l < r) {
-            int m= l+(r-l)/2; // prevent integer overflow
+            int m= (l+r)/2;
             if (nums[m] < target) l= m+1;
             else r= m;
         }
