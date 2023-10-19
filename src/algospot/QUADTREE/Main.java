20cda25b5ad62118db20ac10883af459e5b8379d
jooncco <jooncco.g@gmail.com>
2023-10-19 17:11:38 +0900

    :sparkles: solving: QUADTREE

---
+++ src/algospot/QUADTREE/Main.java
@@ -0,0 +1,71 @@
+package algospot.QUADTREE;
+// https://www.algospot.com/judge/problem/read/QUADTREE
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final FastScanner sc = new FastScanner();
+
+    private static void solve() {
+
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner() {
+            try {
+                br = new BufferedReader(new FileReader("input.txt"));
+                PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
+                System.setOut(out);
+            } catch (Exception e) {
+                br = new BufferedReader(new InputStreamReader(System.in));
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+
+    public static void main(String[] args) {
+        int T = sc.nextInt();
+        for (int t = 1; t <= T; ++t) {
+            if (t > 1)
+                System.out.println();
+            System.out.print("Case #" + t + ": ");
+            solve();
+        }
+    }
+}
