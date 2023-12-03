4c6be836c0dd7cb76571a9f47efe79665d51004f
jooncco <jooncco.g@gmail.com>
2023-12-03 16:14:28 +0900

    :sparkles: solved: p1266

---
+++ src/leetcode/p1266/Solution.java
@@ -0,0 +1,19 @@
+package leetcode.p1266;
+// https://leetcode.com/problems/minimum-time-visiting-all-points/
+
+/**
+ * Math
+ * | Time: O(n)
+ * | Space: O(1)
+ */
+public class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int ans= 0;
+        for (int i=1; i < points.length; ++i) {
+            int x= points[i][0], y= points[i][1];
+            int prevX= points[i-1][0], prevY= points[i-1][1];
+            ans += Math.max(Math.abs(x-prevX), Math.abs(y-prevY));
+        }
+        return ans;
+    }
+}
