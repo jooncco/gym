5202ecc483fc104369205edea59f28f202c8a0b9
jooncco <jooncco.g@gmail.com>
2023-12-06 10:35:52 +0900

    :sparkles: solved: p1716

---
+++ src/leetcode/p1716/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.p1716;
+// https://leetcode.com/problems/calculate-money-in-leetcode-bank/
+
+/**
+ * Math
+ * | Time: O(n/7)
+ * | Space: O(1)
+ */
+public class Solution {
+    public int totalMoney(int n) {
+        int ans= 0;
+        int firstDay= 1;
+        while (n >= 7) {
+            n -= 7;
+            ans += (2*firstDay+6)*7/2;
+            ++firstDay;
+        }
+        return ans + (2*firstDay + (n-1))*n/2;
+    }
+}
