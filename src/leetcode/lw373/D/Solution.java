f584c412ff0f96f3324dd0801a7d7a508a6cd963
jooncco <jooncco.g@gmail.com>
2023-12-06 23:58:43 +0900

    :sparkles: solved: lw373 - D

--- src/leetcode/lw373/D/Solution.java
+++ src/leetcode/lw373/D/Solution.java
@@ -1,32 +1,43 @@
-package leetcode.lw373.D;
+package leetcode.lw366.D;
 // https://leetcode.com/contest/weekly-contest-373/problems/count-beautiful-substrings-ii/
 
+import java.util.*;
+
 /**
- * Dynamic Programming (Bottom-up)
- * 
+ * DP(Bottom-up), Math
+ * | Time: O(n^2/len)
+ * | Space: O(n)
  */
 public class Solution {
     public int beautifulSubstrings(String s, int k) {
         int n= s.length();
-        int[] vCnt= new int[n+1];
-        for (int i= 1; i <= n; ++i) {
-            vCnt[i]= vCnt[i-1];
-            if (isVowel(s.charAt(i-1))) ++vCnt[i];
+        int[] vowelsSoFar= new int[n+1];
+        for (int i=1; i <= n; ++i) {
+            vowelsSoFar[i]= vowelsSoFar[i-1];
+            if (isVowel(s.charAt(i-1))) ++vowelsSoFar[i];
         }
 
+        int len= 2;
+        while (len/2*len/2%k != 0) len += 2;
+        int[] cnt= new int[n+1];
         int ans= 0;
         for (int i=2; i <= n; ++i) {
-            int l= i-2;
-            while (l >= 0) {
-                int totalVowels= vCnt[i]-vCnt[l];
-                if ((totalVowels*2 == i-l && totalVowels*totalVowels%k == 0)) ++ans;
-                l -= 2;
+            if (i < len) continue;
+            int curLen= len;
+            while (curLen <= i) {
+                int vowels= vowelsSoFar[i]-vowelsSoFar[i-curLen];
+                if (vowels == curLen/2) {
+                    cnt[i] += 1 + cnt[i-curLen];
+                    break;
+                }
+                curLen += len;
             }
+            ans += cnt[i];
         }
         return ans;
     }
 
     private boolean isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c =='u';
+        return List.of('a', 'e', 'i', 'o', 'u').contains(c);
     }
 }
