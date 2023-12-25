de7fae306d206104c42b60d335c4cd226f7d484c
jooncco <jooncco.g@gmail.com>
2023-12-25 20:37:17 +0900

    :sparkles: solved: p91

--- src/leetcode/p91/Solution.java
+++ src/leetcode/p91/Solution.java
@@ -1,33 +1,39 @@
 package leetcode.p91;
 // https://leetcode.com/problems/decode-ways/
 
-import java.util.Arrays;
-
+/**
+ * DP (Top-down)
+ * | Time: O(n)
+ * | Space: O(n)
+ */
 public class Solution {
-    private int n;
+    private String str;
     private int[] cache;
 
     public int numDecodings(String s) {
-        if (s.isEmpty()) return 0;
-
-        char[] arr= s.toCharArray();
-        cache= new int[n= arr.length];
-        Arrays.fill(cache, -1);
-        return countWays(arr, 0);
+        str= s;
+        cache= new int[s.length()];
+        for (int i=0; i < s.length(); ++i) {
+            cache[i]= -1;
+        }
+        return numDecodings(0);
     }
 
-    private int countWays(final char[] arr, int idx) {
-        if (idx >= n) return 1;
-        if (arr[idx] == '0') return 0;
-        if (cache[idx] > -1) return cache[idx];
+    private int numDecodings(int idx) {
+        if (idx >= str.length()) return 1;
+        if (cache[idx] != -1) return cache[idx];
 
         cache[idx]= 0;
-        if (idx < n-1) {
-            int twoDigits= Integer.parseInt(""+arr[idx]+arr[idx+1]);
-            if (twoDigits > 0 && twoDigits < 27) cache[idx] += countWays(arr, idx+2);
+        int oneDigit= Integer.parseInt(str.substring(idx, idx+1));
+        if (oneDigit > 0 && oneDigit < 10) {
+            cache[idx] += numDecodings(idx+1);
         }
-        int oneDigit= arr[idx]-'0';
-        if (oneDigit > 0 && oneDigit < 10) cache[idx] += countWays(arr, idx+1);
+        if (idx < str.length()-1) {
+            int twoDigits= Integer.parseInt(str.substring(idx, idx+2));
+            if (twoDigits > 9 && twoDigits < 27) {
+                cache[idx] += numDecodings(idx+2);
+            }
+        }
         return cache[idx];
     }
 }
