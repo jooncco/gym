ae8a7de61ad05b628209722fcd15a11b25674410
jooncco <jooncco.g@gmail.com>
2023-12-27 12:15:15 +0900

    :sparkles: solved: p1578

--- src/leetcode/p1578/Solution.java
+++ src/leetcode/p1578/Solution.java
@@ -1,34 +1,26 @@
 package leetcode.p1578;
 // https://leetcode.com/problems/minimum-time-to-make-rope-colorful/
 
-import java.util.LinkedList;
-import java.util.List;
-
+/**
+ * Greedy
+ * | Time: O(n)
+ * | Space: O(1)
+ */
 public class Solution {
     public int minCost(String colors, int[] neededTime) {
-        int n= colors.length();
-        int ans= 0, l= 0, r= 0, maxVal= neededTime[0];
-        List<Integer> times= new LinkedList<>();
-        times.add(neededTime[0]);
+        int n= neededTime.length;
+        int ans= 0, maxTime= neededTime[0];
         for (int i=1; i < n; ++i) {
-            if (colors.charAt(i) != colors.charAt(i-1)) {
-                if (l < r) {
-                    ans += times.stream().reduce(0, Integer::sum);
-                    ans -= maxVal;
+            if (colors.charAt(i) == colors.charAt(i-1)) {
+                if (maxTime < neededTime[i]) {
+                    ans += maxTime;
+                    maxTime= neededTime[i];
+                } else {
+                    ans += neededTime[i];
                 }
-                l= r= i;
-                maxVal= neededTime[i];
-                times= new LinkedList<>();
-                times.add(neededTime[i]);
             } else {
-                ++r;
-                times.add(neededTime[i]);
-                maxVal= Math.max(maxVal, neededTime[i]);
+                maxTime= neededTime[i];
             }
-        }
-        if (l < r) {
-            ans += times.stream().reduce(0, Integer::sum);
-            ans -= maxVal;
         }
         return ans;
     }
