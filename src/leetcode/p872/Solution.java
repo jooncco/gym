384d66b7c4ba1fa09dd8a64c25e6c133d99f8818
jooncco <jooncco.g@gmail.com>
2024-01-09 09:19:43 +0900

    sparkles: solved: p872

--- src/leetcode/p872/Solution.java
+++ src/leetcode/p872/Solution.java
@@ -1,22 +1,27 @@
 package leetcode.p872;
 // https://leetcode.com/problems/leaf-similar-trees/description/
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
+/**
+ * Trees
+ * | Time: O(n)
+ * | Space: O(n)
+ */
 public class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        List<Integer> leafSeq1= new LinkedList<>(), leafSeq2= new LinkedList<>();
-        appendLeafValues(leafSeq1, root1);
-        appendLeafValues(leafSeq2, root2);
-        return leafSeq1.equals(leafSeq2);
+        List<Integer> leaves1= new ArrayList<>();
+        inOrder(leaves1, root1);
+        List<Integer> leaves2= new ArrayList<>();
+        inOrder(leaves2, root2);
+        return leaves1.equals(leaves2);
     }
 
-    private void appendLeafValues(List<Integer> list, TreeNode node) {
+    private void inOrder(List<Integer> leaves, TreeNode node) {
         if (node == null) return;
-        if (node.left == null && node.right == null) list.add(node.val);
-        appendLeafValues(list, node.left);
-        appendLeafValues(list, node.right);
+        inOrder(leaves, node.left);
+        if (node.left == null && node.right == null) leaves.add(node.val);
+        inOrder(leaves, node.right);
     }
 }
 
