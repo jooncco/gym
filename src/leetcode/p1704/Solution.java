064b3173e356f882cd0a8e8eadffdeeecc77bd56
jooncco <jooncco.g@gmail.com>
2024-01-13 23:28:05 +0900

    :sparkles: solved: p1704

--- src/leetcode/p1704/Solution.java
+++ src/leetcode/p1704/Solution.java
@@ -2,16 +2,21 @@ package leetcode.p1704;
 // https://leetcode.com/problems/determine-if-string-halves-are-alike/
 
 import java.util.List;
+
+/**
+ * Brute Force
+ * | Time: O(n)
+ * | Space: O(1)
+ */
 public class Solution {
     public boolean halvesAreAlike(String s) {
-        int cntA= 0, cntB= 0;
+        int cnt= 0;
         for (int i=0; i < s.length(); ++i) {
             if (isVowel(s.charAt(i))) {
-                if (s.length()/2 > i) ++cntA;
-                else ++cntB;
+                cnt += i < s.length()/2 ? 1 : -1;
             }
         }
-        return cntA == cntB;
+        return cnt == 0;
     }
 
     private boolean isVowel(char c) {
