ef3da6d0d14c53f8eff9d3d2ad65d11747a98855
jooncco <jooncco.g@gmail.com>
2024-01-17 10:03:43 +0900

    :sparkles: solved: p1207

--- src/leetcode/p1207/Solution.java
+++ src/leetcode/p1207/Solution.java
@@ -1,15 +1,25 @@
 package leetcode.p1207;
 // https://leetcode.com/problems/unique-number-of-occurrences/
 
+import java.util.*;
+
+/**
+ * Data Structures(Map)
+ * | Time: O(n)
+ * | Space: O(n)
+ */
 public class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        final int OFFSET= 1000;
-        int[] cnt= new int[2001];
-        for (int num : arr) cnt[num+OFFSET]++;
-        boolean[] countOccurrence= new boolean[1001];
-        for (int i=0; i <= OFFSET+1000; ++i) {
-            if (cnt[i] > 0 && countOccurrence[cnt[i]]) return false;
-            countOccurrence[cnt[i]]= true;
+        Map<Integer, Integer> freq= new HashMap<>();
+        for (int num : arr) {
+            freq.put(num, freq.getOrDefault(num, 0)+1);
+        }
+        Set<Integer> set= new HashSet<>();
+        for (var entry : freq.entrySet()) {
+            if (set.contains(entry.getValue())) {
+                return false;
+            }
+            set.add(entry.getValue());
         }
         return true;
     }
