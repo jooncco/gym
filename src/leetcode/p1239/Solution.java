2a057f4841d6fed01cfe6342b0c525b8ea7ee242
jooncco <jooncco.g@gmail.com>
2024-01-23 09:46:05 +0900

    :sparkles: solved: p1239

--- src/leetcode/p1239/Solution.java
+++ src/leetcode/p1239/Solution.java
@@ -3,80 +3,36 @@ package leetcode.p1239;
 
 import java.util.List;
 
+/**
+ * Brute Force, Bitmask
+ * | Time: O(2^n)
+ * | Space: O(n)
+ */
 public class Solution {
-    private int n, ans;
-    private boolean[] mask;
-
     public int maxLength(List<String> arr) {
-        n= arr.size(); ans= 0;
-        mask= new boolean[26];
-        dfs(arr, 0, 0);
-        return ans;
+        arr= arr.stream().filter(str -> mask(str) != -1).toList();
+        return maxLength(arr, 0, 0, 0);
     }
 
-    private boolean isItselfDistinctive(String str) {
-        boolean[] exist= new boolean[26];
+    private int mask(String str) {
+        int res= 0;
         for (char c : str.toCharArray()) {
-            int idx= c-'a';
-            if (exist[idx]) return false;
-            exist[idx]= true;
+            int bit= 1 << (c - 'a');
+            if ((res & bit) != 0) {
+                return -1;
+            }
+            res |= bit;
         }
-        return true;
+        return res;
     }
 
-    private void dfs(List<String> arr, int idx, int curLen) {
-        if (idx == n) {
-            ans= Math.max(ans, curLen);
-            return;
+    private int maxLength(List<String> arr, int idx, int curMask, int curLen) {
+        if (idx == arr.size()) return curLen;
+        
+        int ret= maxLength(arr, idx+1, curMask, curLen);
+        if ((curMask & mask(arr.get(idx))) == 0) {
+            ret= Math.max(ret, maxLength(arr, idx+1, curMask | mask(arr.get(idx)), curLen+arr.get(idx).length()));
         }
-
-        String curStr= arr.get(idx);
-        boolean exclusive= true;
-        for (char c : curStr.toCharArray()) {
-            if (mask[c-'a']) {
-                exclusive= false;
-                break;
-            }
-        }
-        if (exclusive && isItselfDistinctive(curStr)) {
-            for (char c : curStr.toCharArray()) mask[c-'a']= true;
-            dfs(arr, idx+1, curLen+curStr.length());
-            for (char c : curStr.toCharArray()) mask[c-'a']= false;
-        }
-        dfs(arr, idx+1, curLen);
+        return ret;
     }
 }
-
-//public class Solution {
-//    private int n;
-//    private int[][] cache;
-//
-//    public int maxLength(List<String> arr) {
-//        n= arr.size();
-//        cache= new int[n][1<<26];
-//        for (int i=0; i < n; ++i) Arrays.fill(cache[i], -1);
-//        return find(arr, 0, 0);
-//    }
-//
-//    private int find(final List<String> arr, int idx, int bitMask) {
-//        if (idx == n) return 0;
-//        if (cache[idx][bitMask] > -1) return cache[idx][bitMask];
-//
-//        cache[idx][bitMask]= 0;
-//        String str= arr.get(idx);
-//        boolean exclusive= true;
-//        for (char c : str.toCharArray()) {
-//            if ((bitMask & (1<<(c-'a'))) > 0) {
-//                exclusive= false;
-//                break;
-//            }
-//        }
-//        if (exclusive) {
-//            int newBitMask= bitMask;
-//            for (char c : str.toCharArray()) newBitMask |= (1<<(c-'a'));
-//            cache[idx][bitMask]= Math.max(cache[idx][bitMask], find(arr, idx+1, newBitMask));
-//        }
-//        cache[idx][bitMask]= Math.max(cache[idx][bitMask], find(arr, idx+1, bitMask));
-//        return cache[idx][bitMask];
-//    }
-//}
