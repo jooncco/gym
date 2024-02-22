df2e2fb5117d0dbe8d1781bf1f990105a94376ba
jooncco <jooncco.g@gmail.com>
2024-02-22 18:35:23 +0900

    :sparkle: solved: p997

--- src/leetcode/java/p997/Solution.java
+++ src/leetcode/java/p997/Solution.java
@@ -1,17 +1,23 @@
 package leetcode.java.p997;
 // https://leetcode.com/problems/find-the-town-judge/
 
+/**
+ * Brute force solution
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ */
 public class Solution {
     public int findJudge(int n, int[][] trust) {
-        int[] beingTrustedCount = new int[n];
-        boolean[] voted = new boolean[n];
-        for (int[] vote : trust) {
-            voted[vote[0] - 1] = true;
-            ++beingTrustedCount[vote[1] - 1];
+        int[] trustCount = new int[n + 1];
+        int[] beingtrustedCount = new int[n + 1];
+        for (int[] t : trust) {
+            trustCount[t[0]]++;
+            beingtrustedCount[t[1]]++;
         }
-        for (int i = 0; i < n; ++i) {
-            if (beingTrustedCount[i] == n - 1 && !voted[i])
-                return i + 1;
+        for (int i = 1; i <= n; i++) {
+            if (trustCount[i] == 0 && beingtrustedCount[i] == n - 1) {
+                return i;
+            }
         }
         return -1;
     }
