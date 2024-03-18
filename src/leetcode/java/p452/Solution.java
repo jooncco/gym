a5b652a6294a4834bf7d759e63d3bc1ba93b6636
jooncco <jooncco.g@gmail.com>
2024-03-19 08:29:02 +0900

    :sparkles: solved: p452

--- src/leetcode/java/p452/Solution.java
+++ src/leetcode/java/p452/Solution.java
@@ -3,18 +3,24 @@ package leetcode.java.p452;
 
 import java.util.Arrays;
 
+/**
+ * Greedy
+ * Time: O(n log(n))
+ * Space: O(1)
+ */
 public class Solution {
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points);
-        int ans = 1, R = points[0][1];
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+        int shots = 1;
+        int currentEnd = points[0][1];
         for (int i = 1; i < points.length; ++i) {
-            if (points[i][0] <= R) {
-                R = Math.min(R, points[i][1]);
+            if (points[i][0] <= currentEnd) {
+                currentEnd = Math.min(currentEnd, points[i][1]);
             } else {
-                ++ans;
-                R = points[i][1];
+                ++shots;
+                currentEnd = points[i][1];
             }
         }
-        return ans;
+        return shots;
     }
 }
