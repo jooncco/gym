9b72c139baa5927b062981b60766da7f4c11f181
jooncco <jooncco.g@gmail.com>
2024-04-05 10:04:07 +0900

    :sparkles: solved: p1544

--- src/leetcode/java/p1544/Solution.java
+++ src/leetcode/java/p1544/Solution.java
@@ -1,21 +1,23 @@
 package leetcode.java.p1544;
 // https://leetcode.com/problems/make-the-string-great/
 
+/**
+ * Stack
+ * Time: O(n)
+ * Space: O(n)
+ */
 public class Solution {
-    private StringBuilder sb;
-
     public String makeGood(String s) {
-        sb = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (sb.length() == 0 || goodToAppend(c))
-                sb.append(c);
-            else
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.charAt(0));
+        for (int i = 1; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (!sb.isEmpty() && Math.abs(sb.charAt(sb.length() - 1) - c) == 32) {
                 sb.deleteCharAt(sb.length() - 1);
+            } else {
+                sb.append(c);
+            }
         }
         return sb.toString();
-    }
-
-    private boolean goodToAppend(char c) {
-        return Math.abs(sb.charAt(sb.length() - 1) - c) != 32;
     }
 }
