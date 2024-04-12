71d572ba2862943e6fbc065dcf696e4443996443
jooncco <jooncco.g@gmail.com>
2024-04-12 18:09:33 +0900

    :sparkles: solved: p42

--- src/leetcode/java/p42/Solution.java
+++ src/leetcode/java/p42/Solution.java
@@ -1,31 +1,39 @@
 package leetcode.java.p42;
 // https://leetcode.com/problems/trapping-rain-water/
 
-import java.util.Stack;
+import java.util.*;
 
+/**
+ * Stack
+ * Time: O(n)
+ * Space: O(n)
+ */
 public class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        Stack<Integer[]> walls = new Stack<>();
-        int[] waterLevels = new int[n];
-        for (int i = 0; i < n; ++i) {
-            if (height[i] > 0) {
-                while (!walls.isEmpty() && walls.peek()[0] <= height[i]) {
-                    Integer[] wall = walls.pop();
-                    for (int j = i - 1; j > wall[1]; --j)
-                        waterLevels[j] = wall[0];
+        int water = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(height[0]);
+        int left = height[0];
+        for (int i = 1; i < n; ++i) {
+            if (left >= height[i]) {
+                stack.push(height[i]);
+            } else {
+                int minHeight = Math.min(left, height[i]);
+                while (!stack.isEmpty()) {
+                    water += minHeight - stack.pop();
                 }
-                if (!walls.isEmpty() && walls.peek()[0] > height[i]) {
-                    Integer[] wall = walls.peek();
-                    for (int j = i - 1; j > wall[1]; --j)
-                        waterLevels[j] = height[i];
-                }
-                walls.push(new Integer[] { height[i], i });
+                stack.push(height[i]);
+                left = height[i];
             }
         }
-        int ans = 0;
-        for (int i = 0; i < n; ++i)
-            ans += Math.max(0, waterLevels[i] - height[i]);
-        return ans;
+        // Remaining trapped water on the right side
+        int right = stack.pop();
+        while (!stack.isEmpty()) {
+            int cur = stack.pop();
+            water += Math.max(0, right - cur);
+            right = Math.max(right, cur);
+        }
+        return water;
     }
 }
