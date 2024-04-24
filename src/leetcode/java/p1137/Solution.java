987765571243814e36f5e00e1dc0b5a7a4e84b80
jooncco <jooncco.g@gmail.com>
2024-04-24 14:50:07 +0900

    :sparkles: solved: p1137

--- src/leetcode/java/p1137/Solution.java
+++ src/leetcode/java/p1137/Solution.java
@@ -1,6 +1,11 @@
 package leetcode.java.p1137;
 // https://leetcode.com/problems/n-th-tribonacci-number/
 
+/**
+ * Dynamic Programming (Bottom up)
+ * Time: O(n)
+ * Space: O(1)
+ */
 public class Solution {
     public int tribonacci(int n) {
         if (n == 0)
@@ -9,10 +14,10 @@ public class Solution {
             return 1;
         int a = 0, b = 1, c = 1;
         for (int i = 3; i <= n; ++i) {
-            int tmp = a + b + c;
+            int val = a + b + c;
             a = b;
             b = c;
-            c = tmp;
+            c = val;
         }
         return c;
     }
