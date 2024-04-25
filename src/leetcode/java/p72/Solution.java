4e6ba0a536f802947751fd007555edf2a82031cc
jooncco <jooncco.g@gmail.com>
2024-04-25 13:10:19 +0900

    :sparkles: solved: p72

--- src/leetcode/java/p72/Solution.java
+++ src/leetcode/java/p72/Solution.java
@@ -1,44 +1,47 @@
 package leetcode.java.p72;
 // https://leetcode.com/problems/edit-distance/
 
+/**
+ * Dynamic Programming (Top down)
+ * Time: O(mn)
+ * Space: O(mn)
+ */
 public class Solution {
-    private final int POSITIVE_INFINITY = 999999999;
+    private int m, n;
     private int[][] cache;
-    private String A, B;
-    private int n, m;
+    private static final int INF = Integer.MAX_VALUE;
 
-    public int minDistance(String word1, String word2) {
-        A = word1;
-        B = word2;
-        n = word1.length();
-        m = word2.length();
-        cache = new int[n][m];
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
+    public int minDistance(String A, String B) {
+        m = A.length();
+        n = B.length();
+        cache = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 0; j <= n; ++j) {
                 cache[i][j] = -1;
             }
         }
-        return dp(0, 0);
+        return minDistance(A, B, m, n);
     }
 
-    private int dp(int a, int b) {
-        if (a == n && b == m)
-            return 0;
-        if (a == n)
-            return 1 + dp(a, b + 1);
-        if (b == m)
-            return 1 + dp(a + 1, b);
+    private int minDistance(String A, String B, int a, int b) {
+        if (a == 0)
+            return b;
+        if (b == 0)
+            return a;
         if (cache[a][b] > -1)
             return cache[a][b];
 
-        cache[a][b] = POSITIVE_INFINITY;
-        if (A.charAt(a) != B.charAt(b)) {
-            cache[a][b] = Math.min(cache[a][b], 1 + dp(a + 1, b));
-            cache[a][b] = Math.min(cache[a][b], 1 + dp(a, b + 1));
-            cache[a][b] = Math.min(cache[a][b], 1 + dp(a + 1, b + 1));
+        int ret = INF;
+        if (A.charAt(a - 1) == B.charAt(b - 1)) {
+            ret = minDistance(A, B, a - 1, b - 1);
         } else {
-            cache[a][b] = Math.min(cache[a][b], dp(a + 1, b + 1));
+            // replace
+            ret = Math.min(ret, 1 + minDistance(A, B, a - 1, b - 1));
+            // delete
+            ret = Math.min(ret, 1 + minDistance(A, B, a - 1, b));
+            // insert
+            ret = Math.min(ret, 1 + minDistance(A, B, a, b - 1));
         }
-        return cache[a][b];
+        return cache[a][b] = ret;
     }
 }
