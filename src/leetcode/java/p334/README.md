0c8c7a9b3f2a6fa0a4c3d9ca133b1c89af56ba7e
jooncco <jooncco.g@gmail.com>
2024-11-28 15:15:02 +0900

    :sparkles: solved: p334

---
+++ src/leetcode/java/p334/README.md
@@ -0,0 +1,3 @@
+# lc-334
+
+https://leetcode.com/problems/increasing-triplet-subsequence/
--- src/leetcode/java/p334/Solution.java
+++ src/leetcode/java/p334/Solution.java
@@ -1,24 +1,20 @@
 package leetcode.java.p334;
 // https://leetcode.com/problems/increasing-triplet-subsequence/
 
+/**
+ * DP
+ * Time: O(n)
+ * Space: O(1)
+ */
 public class Solution {
     public boolean increasingTriplet(int[] nums) {
-        int n = nums.length;
-        if (n < 3)
-            return false;
-
-        int firstMin = nums[0], secondMin = Integer.MAX_VALUE;
-        int idx = 0;
-        while (++idx < n) {
-            if (nums[idx] > secondMin)
-                return true;
-            if (nums[idx] == secondMin)
-                continue;
-
-            if (nums[idx] > firstMin) {
-                secondMin = Math.min(secondMin, nums[idx]);
+        int one= Integer.MAX_VALUE, two= Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (one < num) {
+                if (two < num) return true;
+                two= num;
             } else {
-                firstMin = Math.min(firstMin, nums[idx]);
+                one= num;
             }
         }
         return false;
