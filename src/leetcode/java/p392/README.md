c4a23c77fef8caf44c4ffb3ed5123e48b0ea2674
jooncco <jooncco.g@gmail.com>
2024-11-30 00:20:53 +0900

    :sparkles: solved: p392

---
+++ src/leetcode/java/p392/README.md
@@ -0,0 +1,3 @@
+# lc-392
+
+https://leetcode.com/problems/is-subsequence/
--- src/leetcode/java/p392/Solution.java
+++ src/leetcode/java/p392/Solution.java
@@ -2,19 +2,16 @@ package leetcode.java.p392;
 // https://leetcode.com/problems/is-subsequence/
 
 /**
- * Brute Force
- * | Time: O(n)
- * | Space: O(1)
+ * String, Brute Force
+ * Time: O(n)
+ * Space: O(1)
  */
 public class Solution {
     public boolean isSubsequence(String s, String t) {
-        int idx = 0;
+        int idx= 0;
         for (char c : t.toCharArray()) {
-            if (idx == s.length())
-                break;
-            if (s.charAt(idx) == c) {
-                ++idx;
-            }
+            if (idx == s.length()) break;
+            if (s.charAt(idx) == c) ++idx;
         }
         return idx == s.length();
     }
