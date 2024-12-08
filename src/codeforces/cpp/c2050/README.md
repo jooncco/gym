578ec4344647f38b19d1ed65364c9654f9914d79
jooncco <jooncco.g@gmail.com>
2024-12-08 16:37:28 +0900

    :sparkles: solved: cf-2050-a

---
+++ src/codeforces/cpp/c2050/README.md
@@ -0,0 +1,3 @@
+# c-2050
+
+// https://codeforces.com/contest/2050/problem/A
---
+++ src/codeforces/cpp/c2050/a.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios_base::sync_with_stdio(0), cin.tie(0);
+#define bitcount __builtin_popcount
+#define countZeros __builtin_ctz
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef deque<int> di;
+typedef deque<ii> dii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ii> vii;
+typedef priority_queue<int, vi, less<int>> maxHeap;
+typedef priority_queue<int, vi, greater<int>> minHeap;
+
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    string words[60];
+    for (int i=0; i < n; ++i) cin >> words[i];
+    int x= 0, len= 0;
+    for (int i=0; i < n; ++i) {
+        if (len+words[i].length() > m) break;
+        len += words[i].length();
+        ++x;
+    }
+    cout << x << "\n";
+}
+
+int main() {
+    FAST_IO;
+    int t; cin >> t;
+    while (t--) solve();
+}
