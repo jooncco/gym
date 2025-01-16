c0f0ad971c7f0b490ea0edb865c04d553585972c
jooncco <jooncco.g@gmail.com>
2025-01-16 15:28:38 +0900

    :sparkles: solved: lc-2425

---
+++ src/leetcode/cpp/p2425/README.md
@@ -0,0 +1,3 @@
+# lc-2425
+
+https://leetcode.com/problems/bitwise-xor-of-all-pairings/
---
+++ src/leetcode/cpp/p2425/solution.cpp
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/bitwise-xor-of-all-pairings/
+/***
+ * Bitmask, Math
+ * Time: O(n+m)
+ * Space: O(1)
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios_base::sync_with_stdio(0), cin.tie(0);
+#define bitcount __builtin_popcount
+#define countZeros __builtin_ctz
+using ll= long long;
+using ld= long double;
+using pii= pair<int,int>;
+using pll= pair<ll,ll>;
+using vi= vector<int>;
+using vll= vector<ll>;
+using vpii= vector<pii>;
+using vpll= vector<pll>;
+using vvi= vector<vi>;
+using vvll= vector<vll>;
+using vvpll= vector<vpll>;
+using maxHeap= priority_queue<int, vector<int>, less<int>>;
+using minHeap= priority_queue<int, vector<int>, greater<int>>;
+
+class Solution {
+public:
+    int xorAllNums(vi& nums1, vi& nums2) {
+        int xorNums1= 0;
+        if (nums2.size()%2) {
+            for (auto num : nums1) {
+                xorNums1 ^= num;
+            }
+        }
+        int xorNums2= 0;
+        if (nums1.size()%2) {
+            for (auto num : nums2) {
+                xorNums2 ^= num;
+            }
+        }
+        return xorNums1^xorNums2;
+    }
+};
